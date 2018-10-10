36c245730dd6cc163e26c652f9878a881e11d930
Kapil Agarwal <kaagarw@microsoft.com>
2018-10-10 10:13:18 -0700

    Add flags to configure the client and server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,38 +1,98 @@
 extern crate openssl;
-use openssl::ssl::{SslOptions, SslVersion, SslMethod, SslConnector, SslStream};
+use openssl::ssl::{SslOptions, SslVersion, SslMethod, SslConnector, SslStream, SslFiletype};
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::fs::File;
+use std::time::{Instant};
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
 let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
 
-builder.set_ca_file("D:/PersonalCoding/rust/client-server/certificate.pem").unwrap();
-builder.clear_options(SslOptions::NO_TLSV1_3);
-builder.set_min_proto_version(Some(SslVersion::TLS1_3)).unwrap();
+builder.set_private_key_file("D:/PersonalCoding/rust/client-server/client/key.pem", SslFiletype::PEM).unwrap();
+builder.set_certificate_chain_file("D:/PersonalCoding/rust/client-server/client/certificate.pem").unwrap();
+builder.set_ca_file("D:/PersonalCoding/rust/client-server/server/certificate.pem").unwrap();
+
+if args.contains(&String::from("ecc"))
+{
+    builder.set_private_key_file("D:/PersonalCoding/rust/client-server/client/eckey.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("D:/PersonalCoding/rust/client-server/client/eccertificate.pem").unwrap();
+    builder.set_ca_file("D:/PersonalCoding/rust/client-server/server/eccertificate.pem").unwrap();
+}
+else if args.contains(&String::from("server-ecc"))
+{
+    builder.set_ca_file("D:/PersonalCoding/rust/client-server/server/eccertificate.pem").unwrap();
+}
+else if args.contains(&String::from("client-ecc"))
+{
+    builder.set_private_key_file("D:/PersonalCoding/rust/client-server/client/eckey.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("D:/PersonalCoding/rust/client-server/client/eccertificate.pem").unwrap();
+}
+
+if args.contains(&String::from("tls12"))
+{
+    builder.set_max_proto_version(Some(SslVersion::TLS1_2)).unwrap();
+}
+else 
+{
+    builder.clear_options(SslOptions::NO_TLSV1_3);
+    builder.set_min_proto_version(Some(SslVersion::TLS1_3)).unwrap();
+}
+
+//builder.set_ciphersuites("TLS_CHACHA20_POLY1305_SHA256").unwrap();
+
+let file = File::create("D:/PersonalCoding/rust/client-server/client/keyfile.log").unwrap();
+builder.set_keylog_callback(move |_, line| {
+    let _ = writeln!(&file, "{}", line);
+});
+
 let connector = builder.build();
 
 let config = connector.configure().unwrap();
 let version = config.version_str();
 println!("openssl version = {}", version);
 
-// let alpn = config.selected_alpn_protocol().unwrap();
-// println!("alpn = {:?}", alpn);
+// Setup
 
-
-//let stream = TcpStream::connect("google.com:443").unwrap();
-//let mut stream = connector.connect("google.com", stream).unwrap();
-
-let stream = TcpStream::connect("127.0.0.1:8443").unwrap();
-let mut stream = connector.connect("kaagarw", stream).unwrap();
-
-//stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
-handle_server(&mut stream).unwrap();
+let mut iters = 1;
+if args.contains(&String::from("bench"))
+{
+    iters = 10000;
 }
 
-fn handle_server(stream: &mut SslStream<TcpStream>) -> Result<(), std::io::Error> {
-    println!("{}", stream.get_ref().peer_addr()?);
-let mut res = vec![];
-stream.read_to_end(&mut res).unwrap();
-println!("{}", String::from_utf8_lossy(&res));
+// Benchmark
+let start = Instant::now();
+// Execute the routine "iters" times
+for _ in 0..iters {
+    // Code to benchmark using the parameter goes here
+    let stream = TcpStream::connect("127.0.0.1:8444").unwrap();
+    let mut stream = connector.connect("kaagarw", stream).unwrap();
+    handle_server(&mut stream, args.contains(&String::from("verbose"))).unwrap();
+}
+let elapsed = start.elapsed();
+
+// Teardown
+
+// Report elapsed time in nanoseconds to stdout
+println!("{}s {}ns", elapsed.as_secs(), elapsed.subsec_nanos());
+
+}
+
+fn handle_server(stream: &mut SslStream<TcpStream>, verbose: bool) -> Result<(), std::io::Error> {
+    let mut res = vec![];
+    stream.read_to_end(&mut res).unwrap();
+
+    if verbose
+    {
+        println!("{}", stream.get_ref().peer_addr()?);
+        println!("{}", String::from_utf8_lossy(&res));
+        println!("{}", stream.ssl().version_str());
+        if let Some(cipher) = stream.ssl().current_cipher() {
+            println!("{}", cipher.description());
+        }
+    }
+
 Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,32 +1,59 @@
 extern crate openssl;
 
-use openssl::pkcs12::Pkcs12;
-use openssl::ssl::{SslVersion, SslOptions, SslMethod, SslAcceptor, SslStream, SslFiletype};
-use std::fs::File;
-use std::io::{Read, Write};
+use openssl::ssl::{SslVersion, SslOptions, SslMethod, SslAcceptor, SslStream, SslFiletype, SslVerifyMode};
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
-use std::thread;
+use std::{thread, env};
+use std::io::Write;
 
 fn main() {
+let args: Vec<String> = env::args().collect();
 
 let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).expect("Unable to create SslAcceptorBuilder");
-builder.set_private_key_file("D:/PersonalCoding/rust/client-server/key.pem", SslFiletype::PEM).unwrap();
-builder.set_certificate_chain_file("D:/PersonalCoding/rust/client-server/certificate.pem").unwrap();
+
+builder.set_private_key_file("D:/PersonalCoding/rust/client-server/server/key.pem", SslFiletype::PEM).unwrap();
+builder.set_certificate_chain_file("D:/PersonalCoding/rust/client-server/server/certificate.pem").unwrap();
+builder.set_ca_file("D:/PersonalCoding/rust/client-server/client/certificate.pem").unwrap();
+if args.contains(&String::from("ecc"))
+{
+    builder.set_private_key_file("D:/PersonalCoding/rust/client-server/server/eckey.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("D:/PersonalCoding/rust/client-server/server/eccertificate.pem").unwrap();
+    builder.set_ca_file("D:/PersonalCoding/rust/client-server/client/eccertificate.pem").unwrap();
+}
+else if args.contains(&String::from("server-ecc"))
+{
+    builder.set_private_key_file("D:/PersonalCoding/rust/client-server/server/eckey.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("D:/PersonalCoding/rust/client-server/server/eccertificate.pem").unwrap();
+}
+else if args.contains(&String::from("client-ecc"))
+{
+    builder.set_ca_file("D:/PersonalCoding/rust/client-server/client/eccertificate.pem").unwrap();
+}
+
 builder.check_private_key().unwrap();
 builder.clear_options(SslOptions::NO_TLSV1_3);
-builder.set_min_proto_version(Some(SslVersion::TLS1_3)).unwrap();
+builder.set_min_proto_version(Some(SslVersion::TLS1_2)).unwrap();
+
+if args.contains(&String::from("clientcert"))
+{
+    let mut verify_mode = SslVerifyMode::empty();
+    verify_mode.insert(SslVerifyMode::PEER);
+    verify_mode.insert(SslVerifyMode::FAIL_IF_NO_PEER_CERT);
+    builder.set_verify(verify_mode);
+}
+
+println!("{:#?}", builder.options());
 let acceptor = Arc::new(builder.build());
 
-let listener = TcpListener::bind("127.0.0.1:8443").unwrap();
-
+let listener = TcpListener::bind("127.0.0.1:8444").unwrap();
 
 for stream in listener.incoming() {
     if let Ok(stream) = stream {
         let acceptor = acceptor.clone();
+            let verbose = args.contains(&String::from("verbose"));
             thread::spawn(move || {
                 let stream = acceptor.accept(stream).expect("Unable to accept incoming connection.");
-                handle_client(stream);
+                handle_client(stream, verbose);
             });
     }
     else {
@@ -35,9 +62,12 @@ for stream in listener.incoming() {
 }
 }
 
-fn handle_client(mut stream: SslStream<TcpStream>) {
-    let message = String::from("Hello client. This is server !");
-    let buf = message.into_bytes();
-    let written = stream.write(&buf).unwrap_or_default();
-    println!("Written = {}", written);
+fn handle_client(mut stream: SslStream<TcpStream>, verbose: bool) {
+    if verbose
+    {
+        let message = String::from("Hello client. This is server !");
+        let buf = message.into_bytes();
+        let written = stream.write(&buf).unwrap_or_default();
+        println!("Written = {}", written);
+    }
 }
